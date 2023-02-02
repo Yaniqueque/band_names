6ca7ead889147b832867917177adb580ff44e1a6
Santiago Matos <santiagomatosescobar@gmail.com>
2023-02-02 09:03:29 -0400

    Creation of the ListTile, AlertDialog and dismiss

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,11 +1,1 @@
-class Band {
-  String id;
-  String name;
-  int votes;
-
-  Band({required this.id, required this.name, required this.votes});
-
-  factory Band.fromMap(Map<String, dynamic> obj) {
-    return Band(id: obj['id'], name: obj['name'], votes: obj['votes']);
-  }
-}
+export 'band.dart';
